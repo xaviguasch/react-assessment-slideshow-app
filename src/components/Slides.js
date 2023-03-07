15ac714e05210561030a184f04995ec97c7f0f53
xaviguasch <xaviguasch@gmail.com>
2023-03-07 08:58:09 +0100

    solved assessment, passing all the tests

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-function Slides({slides}) {
+function Slides({ slides }) {
+  const [numSlide, setNumSlide] = useState(0)
 
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
-            </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
-        </div>
-    );
-
+  return (
+    <div>
+      <div id='navigation' className='text-center'>
+        <button
+          data-testid='button-restart'
+          className='small outlined'
+          onClick={() => setNumSlide(0)}
+          disabled={numSlide === 0}
+        >
+          Restart
+        </button>
+        <button
+          data-testid='button-prev'
+          className='small'
+          onClick={() => setNumSlide(numSlide - 1)}
+          disabled={numSlide === 0}
+        >
+          Prev
+        </button>
+        <button
+          data-testid='button-next'
+          className='small'
+          onClick={() => setNumSlide(numSlide + 1)}
+          disabled={numSlide === slides.length - 1}
+        >
+          Next
+        </button>
+      </div>
+      <div id='slide' className='card text-center'>
+        <h1 data-testid='title'>{slides[numSlide].title}</h1>
+        <p data-testid='text'>{slides[numSlide].text}</p>
+      </div>
+    </div>
+  )
 }
 
-export default Slides;
+export default Slides
